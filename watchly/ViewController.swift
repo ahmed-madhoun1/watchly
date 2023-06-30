2e2403f31e7ba9bc9ce707c093079a83f9999a06
ahmedmadhoun1 <ahmed2madhoun2@gmail.com>
2023-06-30 17:36:52 +0300

    Initial Commit

---
+++ watchly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  watchly
+//
+//  Created by Ahmed Madhoun on 30/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
